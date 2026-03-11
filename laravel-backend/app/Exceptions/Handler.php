72d1037db05e6650fe0fe08ac17b18ee1ddf28b3
AmazeAI-Network <amaze.onway@gmail.com>
2026-03-12 03:53:34 +0700

    .

--- laravel-backend/app/Exceptions/Handler.php
+++ laravel-backend/app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -32,4 +34,18 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    /**
+     * Override bawaan Laravel agar request API yang gagal auth
+     * TIDAK redirect ke route('login') melainkan langsung JSON 401.
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->is('api/*') || $request->expectsJson()) {
+            return response()->json(['message' => 'Unauthenticated.'], 401);
+        }
+
+        return redirect()->guest('/');
+    }
 }
+
